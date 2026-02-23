a8b5eeefd99c173dd544523c8690b1ff0d56c32b
Aref Moosavi <Aref.Moosavi7813@gmail.com>
2026-02-23 08:41:54 +0330

    add iconbottons

--- lib/main.dart
+++ lib/main.dart
@@ -26,42 +26,53 @@ class _AdvertisingState extends State<Advertising> {
       ],
       supportedLocales: [Locale("fa")],
       home: Scaffold(
-        bottomNavigationBar: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          ),
+        backgroundColor: Colors.black,
+        bottomNavigationBar: SafeArea(
           child: Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.white)),
-            child: BottomNavigationBar(
-              currentIndex: currectindex,
-              onTap: (value) {
-                setState(() {
-                  currectindex = value;
-                });
-              },
-              backgroundColor: Colors.black,
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor: Colors.red,
-              unselectedItemColor: Colors.blue,
-              items: [
-                BottomNavigationBarItem(
-                  label: "صفحه اصلی",
-                  icon: Icon(Icons.home_work),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              child: Container(
+                child: BottomNavigationBar(
+                  currentIndex: currectindex,
+                  onTap: (value) {
+                    setState(() {
+                      currectindex = value;
+                    });
+                  },
+                  backgroundColor: Colors.black,
+                  type: BottomNavigationBarType.fixed,
+                  selectedItemColor: Colors.red,
+                  unselectedItemColor: Colors.blue,
+                  items: const [
+                    BottomNavigationBarItem(
+                      label: "صفحه اصلی",
+                      icon: Icon(Icons.home_work),
+                    ),
+                    BottomNavigationBarItem(
+                      label: "درخواست ها",
+                      icon: Icon(Icons.request_page),
+                    ),
+                    BottomNavigationBarItem(
+                      label: "حساب کاربری",
+                      icon: Icon(Icons.person),
+                    ),
+                    BottomNavigationBarItem(
+                      label: "خروج از حساب",
+                      icon: Icon(Icons.logout),
+                    ),
+                  ],
                 ),
-                BottomNavigationBarItem(
-                  label: "درخواست ها",
-                  icon: Icon(Icons.request_page),
-                ),
-                BottomNavigationBarItem(
-                  label: "جساب کاربری",
-                  icon: Icon(Icons.person),
-                ),
-                BottomNavigationBarItem(
-                  label: "خروج از حساب",
-                  icon: Icon(Icons.logout),
-                ),
-              ],
+              ),
             ),
           ),
         ),
@@ -76,6 +87,37 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("صفحه اصلی"));
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 60,
+            color: Colors.red,
+            child: Row(
+              children: [
+                SizedBox(width: 10),
+                IconButton(onPressed: () {}, icon: Icon(Icons.menu, size: 30)),
+                Text(
+                  "اگهی ها",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.filter_alt_outlined),
+                ),
+                Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: IconButton(onPressed: () {}, icon: Icon(Icons.sort)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

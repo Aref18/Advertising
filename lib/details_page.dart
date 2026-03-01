e365a841ecdc5737600b5ace22d6456c48255728
Aref Moosavi <Aref.Moosavi7813@gmail.com>
2026-03-01 10:02:14 +0330

    coding lrem text and material button

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -37,11 +37,43 @@ class DetailsPage extends StatelessWidget {
             SizedBox(height: 30),
             Expanded(
               child: Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(15),
                 width: double.infinity,
                 height: MediaQuery.of(context).size.height / 1.5,
                 decoration: BoxDecoration(
-                  color: Colors.red,
-                  border: Border.all(color: Colors.blue, width: 4),
+                  borderRadius: BorderRadius.circular(28),
+                  border: Border.all(color: Colors.red, width: 4),
+                ),
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "توضیحات",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '''لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.''',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    Center(
+                      child: MaterialButton(
+                        child: Text("ثبت درخواست"),
+                        color: Colors.red,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
